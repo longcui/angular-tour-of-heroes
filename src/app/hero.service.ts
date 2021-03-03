a20db75819c5b2da7fa11cb1a96c8b87c24bab47
long <long@bityoga.com>
2021-03-03 11:17:44 +0100

    feat: 4. added service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { HEROES } from 'src/app/mock-heros';
+
+import { Hero } from './hero';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+}
